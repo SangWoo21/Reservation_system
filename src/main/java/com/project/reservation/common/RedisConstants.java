b5324f3726949d8596a19c608df45cf29bfc3fb1
juyeongkim <kimjuyeong2@gmail.com>
2026-05-30 20:06:32 +0900

    feat: RedisConstants 클래스 추가 및 대기열 상수 정의

---
+++ src/main/java/com/project/reservation/common/RedisConstants.java
@@ -0,0 +1,8 @@
+package com.project.reservation.common;
+
+public class RedisConstants {
+    public static final String WAITING_QUEUE = "waiting-queue";
+
+    public static final String QUEUE1 = "queue1";
+    public static final String QUEUE1_ACTIVE_KEY = "queue1:active";
+}
